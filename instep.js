c7fb86f43c6f48a5e6b2ea835e0710d4e20bf1e5
Suzanne Onaeko <suzanneonaeko@gmail.com>
2020-11-14 22:49:29 +0000

    Added json

--- instep.js
+++ instep.js
@@ -3,8 +3,32 @@
 const randomise = document.querySelector(".random_id");
 let l = randomise.addEventListener("click", generateid);
 
-function generateid(event) {
+async function generateid(event) {
     event.preventDefault();
     let user_id = document.getElementById("randomidgen").value;
-    document.getElementById("show").innerHTML = user_id;
+    //document.getElementById("show").innerHTML = user_id;
+    const url = "https://dxdunwhca3.execute-api.eu-west-2.amazonaws.com/classIDCreate";
+    const params = {"classID": user_id};
+    const result = await fetch (url,
+       {
+           method: "POST",
+           cache: "no-cache",
+           credentials: "same-origin",
+           headers: {
+               "Content-Type": "application/json",
+           },
+           body: JSON.stringify(params)
+           
+       });
+       if (result.ok) {
+           const dat = await result.json();
+       }
+       else {
+           if(result.status === 400){
+               console.warn(result.statusText);
+           }
+       }
+
+
+
 }
