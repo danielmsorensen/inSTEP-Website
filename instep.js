b62990cc6362f8bf403969691d7f0dd8b932135f
Suzanne Onaeko <suzanneonaeko@gmail.com>
2020-11-15 05:52:14 +0000

    Added dynamic table

--- instep.js
+++ instep.js
@@ -2,8 +2,11 @@
 //To show the randomly generated id
 const randomise = document.querySelector(".random_id");
 let l = randomise.addEventListener("click", generateid);
-const clear = document.querySelector(".game-btn");
+const clear = document.querySelector(".clear-btn");
 let la = clear.addEventListener("click", clearall);
+let game_start = document.querySelector(".game-btn");
+game_start.addEventListener("click", startgame);
+let ingame = false;
 
 const credentials = new AWS.CognitoIdentityCredentials({
     IdentityPoolId: "eu-west-2:793871cb-c8a9-45bf-b20d-97df75ddbce7"
@@ -14,7 +17,8 @@ AWS.config = new AWS.Config({
 });
 
 const lambda = new AWS.Lambda();
-let parsed = "";
+let Names = [];
+let vals = [];
 //let students_names = ["Daniel","Edward","Dan","Suzanne","Eric","Eliza","Gurtrude"];
 let intervalID;
 
@@ -39,12 +43,6 @@ async function generateid(event) {
                 var displayall = document.getElementById("topelement")
                 displayall.innerHTML = "<span> Class ID: "+ 
                 user_id;"</span>"
-                /* for (i=0; i < students_names.length; i++) {
-                    var y = students_names[i];
-                    parsed += y + ": " + "\n";
-                }
-                let p = document.getElementById("show2")
-                p.innerHTML = "<span>" + parsed;"</span>" */
                  
                 intervalID = setInterval(() => {
                     lambda.invoke({
@@ -65,13 +63,20 @@ async function generateid(event) {
                                 const name = names[i];
                                 const value = values[i];
 
-                                parsed += name + ": " + value + "\n";
+                                Names[i] = name;
+                                vals[i] = value;
+                                
+
                             }
-                            let p = document.getElementById("show2")
-                            p.innerHTML = "<span>" + parsed;"</span>"
+                            
+                            //let p = document.getElementById("show2")
+                            //p.innerHTML = "<span>" + parsed;"</span>"
                         }
                     });
                 }, 1000);
+               
+                var ar =[Names, vals];
+                create_table(ar);
 
                 
 
@@ -90,4 +95,36 @@ async function generateid(event) {
 function clearall(event) {
     event.preventDefault();
     clearInterval(intervalID);
+}
+function startgame(event) {
+    event.preventDefault();
+    ingame = !ingame;
+    if (ingame) {
+        console.log("game has started...");
+        document.getElementById("container").style.border = "2rem solid #00FF00";
+
+    }
+    else {
+        console.log("game has ended...")
+        document.getElementById("container").style.border = "none";
+
+    }
+    
+    
+    
+
+}
+function create_table(tbl) {
+    var table = document.querySelector("table tbody");
+
+    var r = tbl[0].map(function(col, i) {
+        return tbl.map(function(row) {
+            return row[i];
+        });
+    });
+
+    r.forEach(function(e){
+        table.innerHTML += "<tr><td>" + e[0] + "</td><td>" + e[1] + "</td></tr>"
+    })
+    
 }
