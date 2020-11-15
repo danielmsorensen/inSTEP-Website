36089230e94b1bee388a3dc536dd086fe8d96567
Suzanne Onaeko <suzanneonaeko@gmail.com>
2020-11-15 00:27:22 +0000

    Added stk

--- instep.js
+++ instep.js
@@ -3,32 +3,34 @@
 const randomise = document.querySelector(".random_id");
 let l = randomise.addEventListener("click", generateid);
 
+const credentials = new AWS.CognitoIdentityCredentials({
+    IdentityPoolId: "eu-west-2:793871cb-c8a9-45bf-b20d-97df75ddbce7"
+});
+AWS.config = new AWS.Config({
+  credentials,
+  region: "eu-west-2"
+});
+
+const lambda = new AWS.Lambda();
+
 async function generateid(event) {
     event.preventDefault();
     let user_id = document.getElementById("randomidgen").value;
     //document.getElementById("show").innerHTML = user_id;
-    const url = "https://dxdunwhca3.execute-api.eu-west-2.amazonaws.com/classIDCreate";
+    //const url = "https://dxdunwhca3.execute-api.eu-west-2.amazonaws.com/classIDCreate";
     const params = {"classID": user_id};
-    const result = await fetch (url,
-       {
-           method: "POST",
-           cache: "no-cache",
-           credentials: "same-origin",
-           headers: {
-               "Content-Type": "application/json",
-           },
-           body: JSON.stringify(params)
-           
-       });
-       if (result.ok) {
-           const dat = await result.json();
-       }
-       else {
-           if(result.status === 400){
-               console.warn(result.statusText);
-           }
-       }
-
-
-
+    lambda.invoke({
+        FunctionName: 'classIDValid',
+        Payload: JSON.stringify({
+            classID: user_id
+        })
+    }, (err, data) => {
+        if (err) {
+            console.log(err, err.stack);
+        }
+        else {
+            console.log(data);
+        }
+    });
+    
 }
